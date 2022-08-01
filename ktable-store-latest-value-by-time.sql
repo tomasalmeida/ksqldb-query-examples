b2dd4cc0abb438fc4027626eb9176689368ba0ae
Tomás Dias Almeida <tomas.almeida@gmail.com>
2022-08-01 19:08:26 +0200

    using timestamp fields

--- ktable-store-latest-value-by-time.sql
+++ ktable-store-latest-value-by-time.sql
@@ -1,21 +1,21 @@
 -- to consume everything from the query
 SET 'auto.offset.reset' = 'earliest';
 
--- input stream with key, value and "time" 
--- for simplicity time is a int value
+-- input stream with key, value and time
 CREATE STREAM stream_input(
   key int, 
   value string,
-  time int
+  time timestamp
 ) WITH (
- kafka_topic = 'stream1', 
+ kafka_topic = 'stream_input', 
  value_format = 'json',
  partitions=1
 );
 
 -- create a table to get the element with biggest time
+-- use unix_timestamp to be able to get max (using int instead of timestamp)
 CREATE TABLE max_by_time AS
-  SELECT key, MAX(time) AS time_max 
+  SELECT key, MAX(UNIX_TIMESTAMP(time)) AS time_max 
   FROM stream_input 
   GROUP BY key
 EMIT CHANGES;
@@ -31,7 +31,7 @@ CREATE STREAM max_values AS
   LEFT JOIN max_by_time t
     ON s.key = t.key
   WHERE 
-    s.time >= t.time_max OR 
+    UNIX_TIMESTAMP(s.time) >= t.time_max OR 
     t.time_max is null
 EMIT CHANGES;
  
@@ -48,13 +48,13 @@ EMIT CHANGES;
 SELECT * FROM max_values_table EMIT CHANGES;
 
 -- in another ksql cli insert values and observe the values
-INSERT INTO stream_input VALUES (1,'ONE',11);
-INSERT INTO stream_input VALUES (2,'TWO',22);
-INSERT INTO stream_input VALUES (3,'THREE',31);
-INSERT INTO stream_input VALUES (4,'FOUR',41);
-INSERT INTO stream_input VALUES (5,'FIVE',51);
-INSERT INTO stream_input VALUES (5,'FIVE_MEGA',101);
-INSERT INTO stream_input VALUES (5,'FIVE',61);
-INSERT INTO stream_input VALUES (5,'FIVE_min',11);
-INSERT INTO stream_input VALUES (5,'FIVE_MEGA_MAX',1111);
-INSERT INTO stream_input VALUES (5,'FIVE',41);
+INSERT INTO stream_input VALUES (1, 'ONE',           '2022-08-01T10:00');
+INSERT INTO stream_input VALUES (2, 'TWO',           '2022-08-01T10:00');
+INSERT INTO stream_input VALUES (3, 'THREE',         '2022-08-01T10:00');
+INSERT INTO stream_input VALUES (4, 'FOUR',          '2022-08-01T10:00');
+INSERT INTO stream_input VALUES (5, 'FIVE',          '2022-08-01T10:00');
+INSERT INTO stream_input VALUES (5, 'FIVE_MEGA',     '2022-08-01T11:00');
+INSERT INTO stream_input VALUES (5, 'FIVE',          '2022-08-01T09:00');
+INSERT INTO stream_input VALUES (5, 'FIVE_min',      '2022-08-01T00:00');
+INSERT INTO stream_input VALUES (5, 'FIVE_MEGA_MAX', '2022-08-01T23:59');
+INSERT INTO stream_input VALUES (5, 'FIVE',          '2022-08-01T10:00');
