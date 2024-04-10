b5b0e8bdfa0b8191179286eac503c2e551800e26
Tomás Dias Almeida <tomas.almeida@gmail.com>
2024-04-10 19:26:52 +0200

    bypassing error 4955

---
+++ select-topic-with-dots.sql
@@ -0,0 +1,42 @@
+-- create using Control center (http://localhost:9021) the topic "topic_underscore.dots"
+
+-- set an schema for the topic
+{
+  "type": "record",
+  "namespace": "com.mycorp.mynamespace",
+  "name": "sampleRecord",
+  "doc": "Sample schema to help you get started.",
+  "fields": [
+    {
+      "name": "f1",
+      "type": "int"
+    },
+    {
+      "name": "f2",
+      "type": "string"
+    }
+  ]
+}
+
+-- produce some data
+kafka-avro-console-producer --bootstrap-server localhost:9092 --topic topic_underscore.dots --property schema.registry.url=http://localhost:8081 --property value.schema.id=1
+{ "f1": 1, "f2" : "one" }
+{ "f1": 2, "f2" : "two" }
+
+
+-- consume the data
+kafka-avro-console-consumer --topic topic_underscore.dots --from-beginning --bootstrap-server localhost:9092 --property schema.registry.url=http://localhost:8081 --property print.schema.ids=true
+
+-- create the stream
+CREATE STREAM NEW_NAME (
+    f1 INT,
+    f2 STRING
+)
+WITH (
+    KAFKA_TOPIC='topic_underscore.dots',
+    VALUE_FORMAT='AVRO'
+);
+
+-- check results
+select * FROM NEW_NAME;
+select * from NEW_NAME where f1 = 1;
